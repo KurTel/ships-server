646b0df105693af352ff4c50b4ab227df6344fcc
Sergey Malov <sergey.malov@eltex.com>
2025-11-04 16:15:45 +0700

    поправил настройку сервера 1

--- src/app.js
+++ src/app.js
@@ -9,7 +9,20 @@ const app = express();
 let secret = 'qwerty';
 app.use(cookieParser(secret));
 app.use(cors({
-    origin: '*',
+    origin: function(origin, callback) {
+        // Разрешаем запросы без origin (например, из Postman)
+        if (!origin) return callback(null, true);
+
+        const allowedOrigins = [
+            'index.html',
+        ];
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
 }));
 app.use(express.json());
